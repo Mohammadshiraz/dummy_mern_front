b7af07d5d20e0ee3ea0245c702fbb3c7727a4c32
Mohammad Shiraz <mohdshiraz980@gmail.com>
2024-10-03 17:50:36 +0530

    link added

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
+      "/api": "https://dummy-mern-vshv.onrender.com/",
     },
   },
   plugins: [react()],
